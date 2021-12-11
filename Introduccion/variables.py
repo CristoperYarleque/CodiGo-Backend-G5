1afd77c02fd9791fd639b09d9c03011a8620b9ec
CristoperYarleque <caypsaturno@gmail.com>
2021-12-10 19:55:40 -0500

    fix: cambios variables

--- Introduccion/variables.py
+++
@@ -1,2 +0,0 @@
-nombre = "Cristoper"
-print(nombre)
---
+++ Sesion1/01-variables.py
@@ -0,0 +1,32 @@
+# variable numerica
+numero = 10 
+numero2 = 10.5
+
+# variable de texto o string
+# numero = "Febrero"
+
+nombre = "Cristoper"
+apellido = "Yarleque"
+
+fecha = 0
+
+soltero = True
+
+print("holaaa :)")
+print(type(numero))
+# str = string
+# int = integer
+# float = float
+# bool = boolean}
+
+# VARIABLES que tienen varios VALORES
+# ARREGLOS ARRAY > LISTA LIST
+edades = [10,12.40,60,"eduardo",14.5,False]
+
+# JSON (JavaScript Object Notation)  Diccionario 
+curso = {
+    "nombre" : "Backend",
+    "nombre" : "Frontend",
+    "dificultad" : "Dificil",
+}
+print(curso["dificultad"])
