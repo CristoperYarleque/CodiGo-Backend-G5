fd32bd626673e4ec33c839e73c00a75f217c8810
CristoperYarleque <caypsaturno@gmail.com>
2021-12-12 03:19:37 -0500

    chore: inicio de operadores

--- Sesion1/01-variables.py
+++ Sesion1/01-variables.py
@@ -12,8 +12,9 @@ fecha = 0
 
 soltero = True
 
-print("holaaa :)")
-print(type(numero))
+# print("holaaa :)")
+# print(type(numero))
+
 # str = string
 # int = integer
 # float = float
@@ -21,12 +22,71 @@ print(type(numero))
 
 # VARIABLES que tienen varios VALORES
 # ARREGLOS ARRAY > LISTA LIST
-edades = [10,12.40,60,"eduardo",14.5,False]
+edades = [10,12.40,60,"cristoper",14.5,False,[1,2]]
+# print(edades[-1]) reversa
+# print(edades[0]) 
+# print(edades[2:4]) rangos
+# print(edades[:3]) hasta
+# print(edades[2:]) desde
 
 # JSON (JavaScript Object Notation)  Diccionario 
 curso = {
     "nombre" : "Backend",
-    "nombre" : "Frontend",
+    # "nombre" : "Frontend",
     "dificultad" : "Dificil",
+    "skills" : [
+        {
+            "nombre" : "Base de datos",
+            "nivel" : "Intermedio"
+        },
+        {
+            "nombre" : "ORM",
+            "nivel" : "Avanzando"
+        }
+    ]
 }
-print(curso["dificultad"])
+# print(curso["skills"][1]["nivel"])
+
+# destructuring
+[primero,segundo,tercero,cuarto,quinto] = ["Enero","Febrero","Marzo","Abril","Mayo"]
+# print(segundo)
+
+personas = [{
+    "nombre" : "Cristoper",
+    "nacionalidad" : "peruano",
+    "hobbies" : [
+        {
+            "nombre" : "Volar drones",
+            "experiencia" : "2 años"
+        },{
+            "nombre" : "Programar",
+            "experiencia" : "1 mes"
+        }
+    ]
+},{
+    "nombre" : "Juliana",
+    "nacionalidad" : "colombiana",
+    "hobbies" : [
+        {
+            "nombre" : "Montar bici",
+            "experiencia" : "4 años"
+        },{
+            "nombre" : "Bases de datos",
+            "experiancia" : "8 meses"
+        }
+    ]
+}
+]
+del personas[0] #eliminar variable , contenido
+# print(personas)
+
+# nacionalidad primera persona
+# print(personas[0]["nacionalidad"])
+
+# hobbies segunda persona
+# print(personas[1]["hobbies"])
+# print(personas[1]["hobbies"][0]["nombre"])
+# print(personas[1]["hobbies"][1]["nombre"])
+
+# experiencia hobbie primera persona
+# print(personas[0]["hobbies"][1]["experiencia"])
