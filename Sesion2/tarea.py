2ea701b9a17c67383e4e4e7c51cf3da6c0fca8e4
CristoperYarleque <caypsaturno@gmail.com>
2022-01-05 18:56:34 -0500

    chore: inicio POO

--- Sesion2/tarea.py
+++
@@ -1,6 +0,0 @@
-# hacer una api en la cual se almacene todos los usuarios de una compañia,
-# se debe guarda el nombre,apellido,area y fecha_inicio,dni
-# vamos a tener un registro y un despido para eliminar el usuario(usar el metodo delete)
-# ademas se puede actualizar el usuario con uno de sus campos(si quiero puedo actulizar el nombre o el apellido,etc)
-# ademas tener una endpoint para devolver el usuario pero segun su dni http://127.0.0.1:5000/usuario/48874584
-# implementar front para este backend
