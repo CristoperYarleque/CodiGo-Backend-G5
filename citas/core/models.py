8e78429f8c22c91bb5b36e64e3610ef8a4b8e6fa
Cristoper Yarleque <caypsaturno@gmail.com>
2022-01-14 22:05:08 -0500

    feat: agregando models django

--- citas/core/models.py
+++ citas/core/models.py
@@ -1,3 +1,63 @@
+from distutils.command.upload import upload
+from pyexpat import model
 from django.db import models
+
+class PersonaModel(models.Model):
+    personaId = models.AutoField(
+        primary_key=True,
+        unique=True,
+        null=False,
+        db_column='id'
+    )
+
+    personaNombre = models.CharField(
+        max_length=50,
+        unique=False,
+        null=False,
+        db_column='nombre'
+    )
+
+    personaApellido = models.CharField(
+        max_length=50,
+        unique=False,
+        null=False,
+        db_column='apellido'
+    )
+
+    personaEmail = models.EmailField(
+        max_length=50,
+        db_column='email',
+        null=False,
+        unique=True
+    )
+
+    personaFechaNacimiento = models.DateField(
+        db_column='fec_nac'
+    )
+
+    opcionesEstadoCivil = [
+        ('SOLTERO','SOLTERO'),
+        ('CASADO','CASADO'),
+        ('VIUDO','VIUDO'),
+        ('DIVORCIADO','DIVORCIADO'),
+        ('COMPLICADO','COMPLICADO'),
+        ('NO_ESPECIFICA','NO_ESPECIFICA')
+    ]
+
+    personaEstadoCivil = models.CharField(
+        choices= opcionesEstadoCivil,
+        db_column='estado_civil',
+        default='NO_ESPECIFICA'
+    )
+
+    personaFoto = models.ImageField(
+        db_column='foto',
+        upload_to='personas/',
+        null=True
+    )
+
+    class Meta:
+        db_table = 'personas'
+        # ordering= ['-email','apellido']
 
 # Create your models here.
