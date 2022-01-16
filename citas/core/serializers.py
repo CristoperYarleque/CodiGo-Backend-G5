bd7623a621d8f4cfd24ca774758dd4f004e71402
Cristoper Yarleque <caypsaturno@gmail.com>
2022-01-15 20:24:27 -0500

    feat: seralizers django

---
+++ citas/core/serializers.py
@@ -0,0 +1,9 @@
+from dataclasses import fields
+from rest_framework import serializers
+from .models import PersonaModel
+
+class PersonasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model= PersonaModel
+        fields='__all__'
+        
