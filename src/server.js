257a5b6cb2aa11cc74c5b8555b82d08858980ba8
Cristoper Yarleque <caypsaturno@gmail.com>
2022-02-11 22:02:52 -0500

    chore: proyecto mongoose

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 import express, { json } from "express";
 import mongoose from "mongoose";
+import { categoriaRouter } from "./routes/categorias.route.js";
+import { imagenRouter } from "./routes/imagen.routes.js";
 
 const app = express();
 app.use(json());
+app.use(categoriaRouter);
+app.use(imagenRouter);
 const PORT = process.env.PORT ?? 3000;
 
 app.listen(PORT, async () => {
   console.log(`servidor corriendo en el puerto ${PORT}`);
   try {
-    await mongoose.connect("mongodb://localhost:27017");
+    await mongoose.connect(process.env.MONGODB);
     console.log("se conecto a la base de datos");
   } catch (error) {
     console.log(error);
