12ce0134d55e1099e1f72d7052999cae228809e4
Cristoper Yarleque <caypsaturno@gmail.com>
2022-02-27 14:11:03 -0500

    chore: actualizacion

--- src/routes/categoria.route.js
+++ src/routes/categoria.route.js
@@ -3,6 +3,6 @@ import { crearCategoria } from "../controller/categoria.controller.js";
 
 export const categoriaRouter = Router();
 
-categoriaRouter.route("/categoria").post(crearCategoria).get().put();
+categoriaRouter.route("/categoria").post(crearCategoria).get();
 
-categoriaRouter.route("/categoria/:id").get();
+categoriaRouter.route("/categoria/:id").get().put();
